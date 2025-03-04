983de232a5f3ddcca621683d05f5302659fb9f5a
dhanushshettigar <dhanushshettigar90@gmail.com>
2025-03-04 17:14:09 +0530

    test1

--- launch/robot_description.launch.py
+++ launch/robot_description.launch.py
@@ -4,26 +4,31 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, PathJoinSubstitution
 from launch.substitutions.launch_configuration import LaunchConfiguration
-
+from launch.conditions import IfCondition
 from launch_ros.actions import Node
 
 
 ARGUMENTS = [
     DeclareLaunchArgument('use_sim_time', default_value='true',
                           choices=['true', 'false'],
-                          description='use_sim_time'),
+                          description='Use simulation time'),
     DeclareLaunchArgument('robot_name', default_value='tcebot',
                           description='Robot name'),
     DeclareLaunchArgument('namespace', default_value='',
                           description='Robot namespace'),
+    DeclareLaunchArgument('launch_rviz', default_value='false',
+                          choices=['true', 'false'],
+                          description='Launch RViz'),
 ]
 
 
 def generate_launch_description():
     pkg_tcebot_description = get_package_share_directory('tcebot_description')
-    xacro_file = PathJoinSubstitution([pkg_tcebot_description, 'urdf','tcebot.urdf.xacro'])
+    xacro_file = PathJoinSubstitution([pkg_tcebot_description, 'urdf', 'tcebot.urdf.xacro'])
     rviz_config_file = PathJoinSubstitution([pkg_tcebot_description, 'rviz', 'config.rviz'])
+    
     namespace = LaunchConfiguration('namespace')
+    launch_rviz = LaunchConfiguration('launch_rviz')
 
     robot_state_publisher = Node(
         package='robot_state_publisher',
@@ -32,8 +37,7 @@ def generate_launch_description():
         output='screen',
         parameters=[
             {'use_sim_time': LaunchConfiguration('use_sim_time')},
-            {'robot_description': Command(['xacro', ' ', xacro_file,
-             ' ', 'namespace:=', namespace])},
+            {'robot_description': Command(['xacro', ' ', xacro_file, ' ', 'namespace:=', namespace])},
         ],
         remappings=[
             ('/tf', 'tf'),
@@ -64,10 +68,12 @@ def generate_launch_description():
             ('/tf', 'tf'),
             ('/tf_static', 'tf_static')
         ],
+        condition=IfCondition(launch_rviz),  # Launch RViz only if 'launch_rviz' is true
     )
 
     ld = LaunchDescription(ARGUMENTS)
     ld.add_action(robot_state_publisher)
     ld.add_action(joint_state_publisher)
     ld.add_action(rviz2)
+
     return ld
